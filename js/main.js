076daee9703e1f57a5756cee56c973e6c1508409
gil <gino.ladowitch@gmail.com>
2020-07-31 21:00:55 +0200

    Secret text in content.js, decrypt, timeout to hide.

--- js/main.js
+++ js/main.js
@@ -1,5 +1,9 @@
+const pPhrase = document.getElementById("pass-phrase");
+const decryptedText = document.getElementById("decrypted-text");
+const timeoutInMiliseconds = 5000;
+let timeoutId; 
+
 const encrypt = () => {
-                
     let encrypted = document.getElementById("encrypted");
     let text = document.getElementById("text-to-encrypt").value;
     let pphrase = document.getElementById("pass-phrase1").value;
@@ -14,3 +18,49 @@ const decrypt = () =>  {
     console.log(`decrypting ${text} with code ${pphrase}...`);
     decrypted.innerHTML = CryptoJS.AES.decrypt(text, pphrase).toString(CryptoJS.enc.Utf8);
 }
+
+function decrypt2(cryptedText, passPhrase) {
+    let uncryptedString = CryptoJS.AES.decrypt(cryptedText, passPhrase).toString(CryptoJS.enc.Utf8);
+    if (uncryptedString.length == 0) {
+        console.log('Decrypt process failed.');
+        return;
+    } else {
+        return uncryptedString;
+    }
+}
+
+function startTimer() { 
+    timeoutId = window.setTimeout(doInactive, timeoutInMiliseconds)
+}
+ 
+function doInactive() {
+    decryptedText.value = "";
+    pPhrase.value = "";
+}
+
+function resetTimer() { 
+    window.clearTimeout(timeoutId)
+    startTimer();
+}
+
+function setupTimers () {
+    document.addEventListener("mousemove", resetTimer, false);
+    document.addEventListener("mousedown", resetTimer, false);
+    document.addEventListener("keypress", resetTimer, false);
+    document.addEventListener("touchmove", resetTimer, false);
+    startTimer();
+}
+
+document.addEventListener('click', function(event) {
+    if (event.target.id == 'decrypt') {
+        if (pPhrase.value.length == 0) {
+            console.log('Pass Phrase needed');
+        } else {
+            decryptedText.value = decrypt2(secretText, pPhrase.value);
+        }
+    }
+});
+
+window.addEventListener("DOMContentLoaded", (event) => {
+    setupTimers();
+});
